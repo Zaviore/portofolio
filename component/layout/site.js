4b5bc57839a7b320ce9333800e416fbd8cfb5570
zamhadi <zamhadi21@gmail.com>
2022-05-12 18:44:25 +0700

    update insight final

--- component/layout/site.js
+++ component/layout/site.js
@@ -85,33 +85,30 @@ const Site = (props) => {
             <meta name='googlebot' content='noindex'></meta>
           </>
         )}
-        <script
-          strategy='afterInteractive'
-          dangerouslySetInnerHTML={{
-            __html: `
-            _linkedin_partner_id = "3892676";
-            window._linkedin_data_partner_ids = 
-            window._linkedin_data_partner_ids || [];                             
-           window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-            (function(l) { 
-                if (!l){window.lintrk = function(a,b) 
-            {window.lintrk.q.push([a,b])};
-            window.lintrk.q=[]} var s = 
-            document.getElementsByTagName("script")[0];
-            var b = document.createElement("script");
-            b.type = "text/javascript";
-            b.async = true;
-            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-            s.parentNode.insertBefore(b, s);})(window.lintrk);`,
-          }}
-        />
+
+        <Script id='facebook-pixel'>
+          {`
+       _linkedin_partner_id = "3892676";
+       window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+       window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+       </script><script type="text/javascript">
+       (function(l) {
+       if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+       window.lintrk.q=[]}
+       var s = document.getElementsByTagName("script")[0];
+       var b = document.createElement("script");
+       b.type = "text/javascript";b.async = true;
+       b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+       s.parentNode.insertBefore(b, s);})(window.lintrk);
+      `}
+        </Script>
         <noscript>
           <img
             height='1'
             width='1'
             style='display:none;'
             alt=''
-            src='https://px.ads.linkedin.com/collect/?pid=3689532&fmt=gif'
+            src='https://px.ads.linkedin.com/collect/?pid=3892676&fmt=gif'
           />
         </noscript>
       </Head>
