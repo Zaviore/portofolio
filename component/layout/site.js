5dfeffd5fbc4f9347ffab146d9ce0847e87ee96a
zamhadi <zamhadi21@gmail.com>
2022-05-12 18:23:34 +0700

    update insight final

--- component/layout/site.js
+++ component/layout/site.js
@@ -84,6 +84,35 @@ const Site = (props) => {
             <meta name='googlebot' content='noindex'></meta>
           </>
         )}
+        <Script
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+            _linkedin_partner_id = "3892676";
+            window._linkedin_data_partner_ids = 
+            window._linkedin_data_partner_ids || [];                             
+           window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+            (function(l) { 
+                if (!l){window.lintrk = function(a,b) 
+            {window.lintrk.q.push([a,b])};
+            window.lintrk.q=[]} var s = 
+            document.getElementsByTagName("script")[0];
+            var b = document.createElement("script");
+            b.type = "text/javascript";
+            b.async = true;
+            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+            s.parentNode.insertBefore(b, s);})(window.lintrk);`,
+          }}
+        />
+        <noscript>
+          <img
+            height='1'
+            width='1'
+            style='display:none;'
+            alt=''
+            src='https://px.ads.linkedin.com/collect/?pid=3689532&fmt=gif'
+          />
+        </noscript>
       </Head>
 
       <div>
