d81c3d6626be6da881eded7c19c381c8cf953e55
zamhadi <zamhadi21@gmail.com>
2022-05-18 11:02:15 +0700

    update testing

--- component/layout/site.js
+++ component/layout/site.js
@@ -6,7 +6,7 @@ import SiteHeader from "./header";
 import SiteFooter from "./footer";
 import { useRouter } from "next/router";
 import Script from "next/script";
-// import LinkedInTag from "react-linkedin-insight";
+import LinkedInTag from "react-linkedin-insight";
 
 const SEO = {
   title: "Homepage",
@@ -23,7 +23,7 @@ const SEO = {
 const Site = (props) => {
   const { seo = SEO, isHome = false, isLogin = false, children } = props;
   const [show, setShow] = useState(false);
-  console.log(show, "<<Apa");
+
   useEffect(() => {
     if (window.scrollY > 0) {
       setShow(true);
@@ -32,9 +32,12 @@ const Site = (props) => {
     }
   }, [show]);
   // const disabled = !user.allowsThirdPartyCookies();
-  // LinkedInTag.init(3892676, "dc", disabled);
-  // LinkedInTag.track(3892676);
   const router = useRouter();
+
+  useEffect(() => {
+    LinkedInTag.init(3892676, "dc", disabled);
+    LinkedInTag.track(3892676);
+  }, []);
 
   useEffect(() => {
     import("react-facebook-pixel")
@@ -48,6 +51,7 @@ const Site = (props) => {
         });
       });
   }, [router.events]);
+
   return (
     <>
       <Head>
@@ -85,32 +89,6 @@ const Site = (props) => {
             <meta name='googlebot' content='noindex'></meta>
           </>
         )}
-
-        <Script>
-          {`
-       _linkedin_partner_id = "3892676";
-       window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
-       window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-       </script><script type="text/javascript">
-       (function(l) {
-       if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
-       window.lintrk.q=[]}
-       var s = document.getElementsByTagName("script")[0];
-       var b = document.createElement("script");
-       b.type = "text/javascript";b.async = true;
-       b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-       s.parentNode.insertBefore(b, s);})(window.lintrk);
-      `}
-        </Script>
-        <noscript>
-          <img
-            height='1'
-            width='1'
-            style='display:none;'
-            alt=''
-            src='https://px.ads.linkedin.com/collect/?pid=3892676&fmt=gif'
-          />
-        </noscript>
       </Head>
 
       <div>
